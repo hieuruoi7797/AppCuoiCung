73be5ff5bdeeee8c6dfb89cca3983bc59470fda1
DESKTOP-VDLDI3J\ctrls <trantrunghieuse@gmail.com>
2017-11-28 19:32:59 +0700

    update login

--- MyLocation/app/src/main/java/gloryhunter/mylocation/LoginActivity.java
+++ MyLocation/app/src/main/java/gloryhunter/mylocation/LoginActivity.java
@@ -1,8 +1,10 @@
 package gloryhunter.mylocation;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,6 +15,9 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText etName;
@@ -22,6 +27,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private Button btLogin;
     private Button btRegister;
     private TextView tvDetail;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+
     private FirebaseAuth firebaseAuth;
 
     @Override
@@ -41,34 +49,78 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         btRegister.setOnClickListener(this);
 
         firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+      
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.bt_register:{
-                firebaseAuth.signInWithEmailAndPassword(
+                firebaseAuth.createUserWithEmailAndPassword(
                         etEmail.getText().toString(),
                         etPass.getText().toString()
                 ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()){
-                            Toast.makeText(LoginActivity.this,"Register successful",
-                                    Toast.LENGTH_SHORT).show();
-                        }else {
-                            Toast.makeText(LoginActivity.this,
-                                    task.getException().getMessage(),
+                            firebaseAuth.getCurrentUser().sendEmailVerification()
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()){
+                                                Toast.makeText(LoginActivity.this,
+                                                        "thanhcongCMNR",
+                                                        Toast.LENGTH_SHORT).show();
+
+                                                UserProfileChangeRequest userProfileChangeRequest = new UserProfileChangeRequest.Builder()
+                                                        .setDisplayName(etName.getText().toString())
+                                                        .setPhotoUri(Uri.parse(""))
+                                                        .build();
+                                                firebaseAuth.getCurrentUser().updateProfile(userProfileChangeRequest);
+                                            }else{
+                                                Toast.makeText(LoginActivity.this,
+                                                        task.getException().getMessage(),
+                                                        Toast.LENGTH_SHORT).show();
+                                            }
+                                        }
+                                    });
+                            UserMode userMode = new UserMode(
+                                    etName.getText().toString(),
+                                    etEmail.getText().toString(),
+                                    etPhone.getText().toString()
+                            );
+                            databaseReference.push().setValue(userMode);
+
+                        }else{
+                            Toast.makeText(LoginActivity.this, task.getException().getMessage(),
                                     Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
+
                 break;
             }
             case R.id.bt_login:{
-
+                        firebaseAuth.signInWithEmailAndPassword(
+                                etEmail.getText().toString(),
+                                etPass.getText().toString()
+                        ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()){
+                                    Toast.makeText(LoginActivity.this,"Login successful",
+                                            Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(LoginActivity.this,MainActivity.class));
+                                }else {
+                                    Toast.makeText(LoginActivity.this,
+                                            task.getException().getMessage(),
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                    }
                 break;
             }
         }
     }
-}
---
+++ MyLocation/app/src/main/java/gloryhunter/mylocation/UserMode.java
@@ -0,0 +1,20 @@
+package gloryhunter.mylocation;
+
+/**
+ * Created by ctrls on 28/11/2017.
+ */
+
+public class UserMode {
+    public String userName;
+    public String email;
+    public String phoneNumber;
+
+    public UserMode() {
+    }
+
+    public UserMode(String userName, String email, String phoneNumber) {
+        this.userName = userName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+}
